f25a956df86dccedc2a4ddb2438922e8e7d94838
anthonyhartin <82573614+anthonyhartin@users.noreply.github.com>
2022-04-04 11:01:41 +0200

    Add files via upload

---
+++ usernetes_install_part1.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# first script for usernetes install
+#---------------------------------------------------
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+echo "Usernetes install requires two scripts to be run with an intervening reboot to refresh user cgroups"
+echo "This is the first script. Run it as an ordinary user. Sudo access is required."; fi
+#---------------------------------------------------
+workdir=$PWD
+workuser=$USER
+vercheck=`echo \`uname -r | cut -c 1-4\` 4.18 | awk '{if ($1<$2) print "fail"}'`
+portcheck=`sudo netstat -pna | grep 6443`                                  
+# ---------------first check some prerequisites-----------------
+if ! compgen -u | grep -q $workuser; then echo "stopping because the user $workuser does not exist"; exit; fi
+if ! sudo -l | grep -q " ALL"; then echo "stopping because you don't have sudo access. This is required"; exit; fi
+[ ! -f /sys/fs/cgroup/cgroup.controllers ] && { echo "stopping because cgroups do not exist. please install"; exit; }
+[ "$vercheck" = "fail" ] && { echo "stopping because your kernel version is less than 4.18. please upgrade"; exit; }
+[ -z `whereis newuidmap | gawk '{print \$2}'` ] && { echo "stopping because there is no newuidmap. please install"; exit; }
+[ -z `whereis newgidmap | gawk '{print \$2}'` ] && { echo "stopping because there is no newuidmap. please install"; exit; }
+sudo dnf -y install dnf-plugins-core wget bzip2 fuse iptables-legacy conntrack-tools                  # prerequisites for usernetes
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo            # docker repo
+sudo dnf -y install docker-ce docker-ce-cli containerd.io                                             # docker and kubernetes utilities
+if ! grep -q docker /etc/group; then sudo groupadd docker;fi                                          #
+sudo usermod -aG docker $workuser                                                                     # allow docker to be used by the non root user
+sudo systemctl enable docker; sudo systemctl start docker                                             # make sure docker always starts up
+sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=1"                           # enable cgroup v2 at boot time
+sudo mkdir -p /etc/systemd/system/user@.service.d                                                     # we are going to allow the user to use all controllers
+cat <<EOF > delegate.conf                                                                             # create config file
+[Service]
+Delegate=yes
+EOF
+sudo mv delegate.conf /etc/systemd/system/user@.service.d/                                            # deploy config file
+sudo systemctl daemon-reload
+echo "Now reboot the machine in order to refresh user cgroups"
---
+++ usernetes_install_part2.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# Second script for usernetes install
+#---------------------------------------------------
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+echo "Usernetes install requires two scripts to be run with an intervening reboot to refresh user cgroups"
+echo "This is the second script. Run it as an ordinary user. Sudo access is required."; fi
+#---------------------------------------------------
+workdir=$PWD
+workuser=$USER
+vercheck=`echo \`uname -r | cut -c 1-4\` 4.18 | awk '{if ($1<$2) print "fail"}'`
+portcheck=`sudo netstat -pna | grep 6443`                                  
+# ---------------first check some prerequisites-----------------
+if ! compgen -u | grep -q $workuser; then echo "stopping because the user $workuser does not exist"; exit; fi
+if ! sudo -l | grep -q " ALL"; then echo "stopping because you don't have sudo access. This is required"; exit; fi
+[ ! -f /sys/fs/cgroup/cgroup.controllers ] && { echo "stopping because cgroups do not exist. please install"; exit; }
+[ "$vercheck" = "fail" ] && { echo "stopping because your kernel version is less than 4.18. please upgrade"; exit; }
+[ -z `whereis newuidmap | gawk '{print \$2}'` ] && { echo "stopping because there is no newuidmap. please install"; exit; }
+[ -z `whereis newgidmap | gawk '{print \$2}'` ] && { echo "stopping because there is no newuidmap. please install"; exit; }
+if ! cat /sys/fs/cgroup/user.slice/user-$(id -u).slice/user@$(id -u).service/cgroup.subtree_control | grep cpu | grep -q io
+then echo "stopping because user cgroups don't include cpu and io. Have you run the first script and rebooted?"; exit; fi        # check cgroups, should contain cpu and io
+wget https://github.com/rootless-containers/usernetes/releases/download/v20211108.0/usernetes-x86_64.tbz                         # get the latest usernetes release
+tar xjf usernetes-x86_64.tbz
+rm usernetes-x86_64.tbz
+sudo install -o root -g root -m 0755 usernetes/bin/kubectl /usr/local/bin/kubectl
+cd usernetes
+sed '/kubectl -n kube-system wait/ s/./#&/' install.sh > tmp.dat; mv tmp.dat install.sh; chmod +x install.sh
+./install.sh --cri=containerd                                                                                                    # the usernetes install
+docker run -td --name usernetes-node -p 127.0.0.1:6443:6443 --privileged ghcr.io/rootless-containers/usernetes --cri=containerd
+docker cp usernetes-node:/home/user/.config/usernetes/master/admin-localhost.kubeconfig docker.kubeconfig
+#export KUBECONFIG=./docker.kubeconfig
+sudo loginctl enable-linger                            # start user services automatically on system startup
+echo "usernetes install completed. "
+echo "add \"export KUBECONFIG=/home/fedora/.config/usernetes/master/admin-localhost.kubeconfig\" to your persistent environment variables."
