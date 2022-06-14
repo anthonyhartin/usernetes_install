6e4c209f316103b6de90941f084d1e56d72bd398
Anthony Hartin <tony.hartin@gmail.com>
2022-06-14 08:59:20 +0200

    change cgroup v2 confirmation

--- usernetes_install_part2.sh
+++ usernetes_install_part2.sh
@@ -15,7 +15,8 @@ if ! cat /sys/fs/cgroup/user.slice/user-$(id -u).slice/user@$(id -u).service/cgr
 then echo "stopping because the user cgroups do not allow control of the cpu and io."; exit; fi                                          # check cgroups, should contain cpu and io
 if ! compgen -u | grep -q $workuser; then echo "stopping because the user $workuser does not exist"; exit; fi
 if ! sudo -l | grep -q " ALL"; then echo "stopping because you don't have sudo access. This is required"; exit; fi
-[ ! -f /sys/fs/cgroup/cgroup.controllers ] && { echo "stopping because cgroups do not exist. please install"; exit; }
+if ! grep -q cgroup2 /proc/filesystems; then echo "stopping because cgroups v2 does not exist. please install"; exit; fi
+#[ ! -f /sys/fs/cgroup/cgroup.controllers ] && { echo "stopping because cgroups do not exist. please install"; exit; }
 [ "$vercheck" = "fail" ] && { echo "stopping because your kernel version is less than 4.18. please upgrade"; exit; }
 [ -z `whereis newuidmap | gawk '{print \$2}'` ] && { echo "stopping because there is no newuidmap. please install"; exit; }
 [ -z `whereis newgidmap | gawk '{print \$2}'` ] && { echo "stopping because there is no newuidmap. please install"; exit; }
