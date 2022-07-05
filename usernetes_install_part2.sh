5cbb8529e34e732354dcc47e8ca8b3c06961a5bd
Anthony Hartin <tony.hartin@gmail.com>
2022-07-05 11:46:18 +0200

    comment out docker

--- usernetes_install_part2.sh
+++ usernetes_install_part2.sh
@@ -33,11 +33,10 @@ while [ $result -ne 0 ]; do
         sleep 5                                                                                # otherwise, wait a bit before trying again
     fi
 done
-docker run -td --name usernetes-node -p 127.0.0.1:6443:6443 --privileged ghcr.io/rootless-containers/usernetes --cri=containerd
-export KUBECONFIG=
-docker cp usernetes-node:/home/user/.config/usernetes/master/admin-localhost.kubeconfig docker.kubeconfig
-export KUBECONFIG=$workdir/docker.kubeconfig
+#docker run -td --name usernetes-node -p 127.0.0.1:6443:6443 --privileged ghcr.io/rootless-containers/usernetes --cri=containerd
+#docker cp usernetes-node:/home/user/.config/usernetes/master/admin-localhost.kubeconfig docker.kubeconfig
+#export KUBECONFIG=$workdir/docker.kubeconfig
 sudo loginctl enable-linger                            # start user services automatically on system startup
-echo "Docker serves the usernetes node. Set KUBECONFIG to "$workdir/docker.kubeconfig
+#echo "Docker serves the usernetes node. Set KUBECONFIG to "$workdir/docker.kubeconfig
 echo "usernetes install completed. "
 
